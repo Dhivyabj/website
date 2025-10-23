4c345aaa4adc069cef8799a2875189dc1fab0bfa
Dhivya Dharshni <bjdhivyadharshni@gmail.com>
2025-10-23 12:35:08 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import {FaGithub, FaLinkedin, FaTelegram} from "react-icons/fa"
+import logo from "../assets/dhivyaLogo.webp"
+ 
+const  Navbar = () =>  {
+    return (
+      <nav className="flex items-center justify-between py-6"> 
+        <div className="flex flex-shrink-0 items-center">
+          <a href="/" aria-label="Home">
+              <img src={logo} className="mx-2" width={50} height={33}
+              alt="Logo"/>
+          </a>
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <a href="https://www.linkedin.com/in/dhivyadharshni-bj/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn">
+              <FaLinkedin />
+          </a>
+           <a href="https://github.com/Dhivyabj"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub">
+              <FaGithub />
+            </a>
+             <a href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Telegram">
+              <FaTelegram />
+            </a>
+
+
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar
+
